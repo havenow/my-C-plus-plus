0be585c90051e8ff7bcf2d2d920c402cba37155a
havenow <715667426@qq.com>
2021-01-12 18:11:12 +0800

    多继承不同继承顺序类对象布局不同

---
+++ C++对象内存布局(单继承 多继承 菱形继承 虚继承)/7.cpp
@@ -0,0 +1,36 @@
+
+class BaseA
+{
+public:
+	BaseA() = default;
+	virtual ~BaseA() = default;
+	void FuncA() {}
+	virtual void FuncB(){}
+	int a;
+	int b;
+};
+
+class BaseB
+{
+public:
+	BaseB() = default;
+	virtual ~BaseB() = default;
+	void FuncA() {}
+	virtual void FuncC(){}
+	int a;
+	int b;
+};
+
+class Derive : public BaseB, public BaseA 
+{
+public:
+	void FuncB() override {}
+	void FuncC() override {}
+};
+
+int main()
+{
+	BaseA a;
+	Derive d;
+	return 0;
+}
